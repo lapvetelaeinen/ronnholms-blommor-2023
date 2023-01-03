c2133dbea98749538da37a82ef2ee83835a1e139
Filip Lapveteläinen <filip.lapvetelainen@gmail.com>
2023-01-03 09:16:30 +0100

    white bg

--- pages/om-oss.js
+++ pages/om-oss.js
@@ -25,7 +25,7 @@ export default function Om() {
           </div>
         </div> */}
 
-        <div className="w-[100vw] h-[200px] relative">
+        <div className="w-[100vw] h-[200px] relative bg-white">
           <Image
             src={om2}
             fill
